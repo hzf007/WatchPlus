0fd4eb781d9fae7e35403353e040511fdce09642
hzf <760646681@qq.com>
2017-02-23 11:51:14 +0800

    gengxin

--- paint2d/src/main/java/com/aspsine/mobi/paint2d/MainActivity.java
+++ paint2d/src/main/java/com/aspsine/mobi/paint2d/MainActivity.java
@@ -13,8 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         new ArrayList<>();
-        maeasureHight(1);
-        measureWidth(1);
     }
 
     private int measureWidth(int measureSpec) {
